4348776bf3585c6e84359fb0bebcbe940b5efdb3
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-09-08 16:43:53 +0100

    start making model picker

--- packages/lib/src/LLMCalculator.wc.svelte
+++ packages/lib/src/LLMCalculator.wc.svelte
@@ -1,9 +1,45 @@
 <svelte:options tag="llm-calc"/>
 
 <script lang="ts">
+  enum ModelArch {
+    Llama = 'LLaMA',
+    Llama2 = 'Llama 2',
+  }
+  const modelArchs: ModelArch[] = Object.values(ModelArch);
+  let modelArch: ModelArch = ModelArch.Llama;
 </script>
 
-<div>whatever</div>
+<form>
+  <fieldset>
+    <legend>Model</legend>
+    <label>
+      Architecture
+      <select
+        value={modelArch}
+      >
+        {#each modelArchs as modelArch_}
+          <option value={modelArch_}>{modelArch_}</option>
+        {/each}
+      </select>
+    </label>
+    
+  </fieldset>
+</form>
+
+<table>
+  <thead>
+    <tr>
+      <th>Head0</th>
+      <th>Head1</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Bod0</td>
+      <td>Bod1</td>
+    </tr>
+  </tbody>
+</table>
 
 <style>
 </style>
