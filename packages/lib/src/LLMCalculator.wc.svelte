6fb7cb7d92196ca0c2d2e1a281d52c40cd193017
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-09-14 17:48:53 +0100

    model fieldset

--- packages/lib/src/LLMCalculator.wc.svelte
+++ packages/lib/src/LLMCalculator.wc.svelte
@@ -1,56 +1,9 @@
 <svelte:options tag="llm-calc"/>
 
-<script lang="ts">
-  import { ModelSuite } from './model-model/model-suite';
-  import { models, type ModelConfig } from './model-model/model-db';
-  const modelSuites: ModelSuite[] = Object.values(ModelSuite);
-  let modelSuite: ModelSuite = ModelSuite.Llama1;
-  type ModelChoiceBySuite = {
-    [suite in ModelSuite]: [keyof (typeof models)[suite]]
-  }
-  let modelChoiceBySuite: ModelChoiceBySuite = Object.values(ModelSuite).reduce(<A extends ModelSuite>(acc: Partial<ModelChoiceBySuite>, arch: A): Partial<ModelChoiceBySuite> => {
-    const possibleKeys: keyof (typeof models)[A] = (Object.keys(models[arch]) as unknown as keyof (typeof models)[A]);
-    acc[arch] = possibleKeys[0];
-    return acc;
-  }, {}) as ModelChoiceBySuite;
-  let modelConfig: ModelConfig;
-  $: modelConfig = models[modelSuite][modelChoiceBySuite[modelSuite] as unknown as keyof typeof models[typeof modelSuite]];
-</script>
-
 <div class="llm-calc-container">
   <form>
     <div class="row">
-      <fieldset>
-        <legend>Model</legend>
-        <label class="row">
-          Architecture
-          <select bind:value={modelSuite}>
-            {#each modelSuites as modelSuite_}
-              <option value={modelSuite_}>{modelSuite_}</option>
-            {/each}
-          </select>
-        </label>
-        <label class="row">
-          Model
-          <select
-            bind:value={modelChoiceBySuite[modelSuite]}
-          >
-            {#each Object.keys(models[modelSuite]) as model_}
-              <option value={model_}>{model_}</option>
-            {/each}
-          </select>
-        </label>
-        <div class="row">
-          <div>
-            <h4 class="definition-list-header">Config</h4>
-            <model-config-dl conf={modelConfig}/>
-          </div>
-          <div>
-            <h4 class="definition-list-header">Params</h4>
-            <model-params conf={modelConfig}/>
-          </div>
-        </div>
-      </fieldset>
+      <model-fieldset/>
       <optimizer-fieldset/>
     </div>
   </form>
@@ -97,13 +50,5 @@
   .def-col {
     background-color: aliceblue;
     font-weight: 700;
-  }
-
-  /* TODO: remove this when we embed component into blog, which probably doesn't have a dark mode */
-  @media (prefers-color-scheme: dark) {
-    .llm-calc-container {
-      background-color: white;
-      color: black;
-    }
   }
 </style>
---
+++ packages/lib/src/model-view/ModelFieldset.wc.svelte
@@ -0,0 +1,61 @@
+<svelte:options tag="model-fieldset"/>
+
+<script lang="ts">
+  import { ModelSuite } from '../model-model/model-suite';
+  import { models, type ModelConfig } from '../model-model/model-db';
+  const modelSuites: ModelSuite[] = Object.values(ModelSuite);
+  let modelSuite: ModelSuite = ModelSuite.Llama1;
+  type ModelChoiceBySuite = {
+    [suite in ModelSuite]: [keyof (typeof models)[suite]]
+  }
+  let modelChoiceBySuite: ModelChoiceBySuite = Object.values(ModelSuite).reduce(<A extends ModelSuite>(acc: Partial<ModelChoiceBySuite>, arch: A): Partial<ModelChoiceBySuite> => {
+    const possibleKeys: keyof (typeof models)[A] = (Object.keys(models[arch]) as unknown as keyof (typeof models)[A]);
+    acc[arch] = possibleKeys[0];
+    return acc;
+  }, {}) as ModelChoiceBySuite;
+  let modelConfig: ModelConfig;
+  $: modelConfig = models[modelSuite][modelChoiceBySuite[modelSuite] as unknown as keyof typeof models[typeof modelSuite]];
+</script>
+
+<fieldset>
+  <legend>Model</legend>
+  <label class="row">
+    Architecture
+    <select bind:value={modelSuite}>
+      {#each modelSuites as modelSuite_}
+        <option value={modelSuite_}>{modelSuite_}</option>
+      {/each}
+    </select>
+  </label>
+  <label class="row">
+    Model
+    <select
+      bind:value={modelChoiceBySuite[modelSuite]}
+    >
+      {#each Object.keys(models[modelSuite]) as model_}
+        <option value={model_}>{model_}</option>
+      {/each}
+    </select>
+  </label>
+  <div class="row">
+    <div>
+      <h4 class="definition-list-header">Config</h4>
+      <model-config-dl conf={modelConfig}/>
+    </div>
+    <div>
+      <h4 class="definition-list-header">Params</h4>
+      <model-params conf={modelConfig}/>
+    </div>
+  </div>
+</fieldset>
+
+<style>
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1em;
+  }
+  .definition-list-header {
+    margin-bottom: 0;
+  }
+</style>
