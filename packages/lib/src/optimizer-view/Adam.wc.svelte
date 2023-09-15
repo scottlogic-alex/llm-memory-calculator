234c7da02ac610a823c11c877ed12d807eeaf970
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-09-15 10:03:30 +0100

    Adam dtype picker

--- packages/lib/src/optimizer-view/Adam.wc.svelte
+++ packages/lib/src/optimizer-view/Adam.wc.svelte
@@ -3,10 +3,20 @@
 <script lang="ts">
     import { type Writable, type Readable } from 'svelte/store';
     import { type AdamConfig, AdamDataType } from '../optimizer-model/adam-configs';
-    import { adamConfig } from '../optimizer-model/adam-store';
-    console.log($adamConfig);
+    import { adamConfig, adamDtype, adam8BitDoubleQuant } from '../optimizer-model/adam-store';
 </script>
 
 <div>
     Hi, I'm Adam
+    <div class="column">
+        <label><input type="radio" bind:group={$adamDtype} value={AdamDataType.float32}/>32-bit</label>
+        <label><input type="radio" bind:group={$adamDtype} value={AdamDataType.int8}/>8-bit</label>
+    </div>
 </div>
+
+<style>
+  .column {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
