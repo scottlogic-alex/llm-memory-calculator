7a1a56fedc4db5c57197e4b3047505c0fadb2ea2
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-09-21 17:43:27 +0100

    the compute plan

--- packages/lib/src/compute-view/ComputeFieldset.wc.svelte
+++ packages/lib/src/compute-view/ComputeFieldset.wc.svelte
@@ -1,6 +1,18 @@
 <svelte:options tag="compute-fieldset"/>
 
 <script lang="ts">
+    /*
+    for compute we'll need:
+    - mixed-precision
+      - entails taking a half-precision copy of the master weights
+    - fixed-precision
+        - no copy required
+    - check whether master weights are already in the precision needed (i.e. no compute copy required) for:
+      - LN
+      - embedding
+      - unembedding
+    - we could consider gradients to be the same size as the compute buffers
+    */
 </script>
 
 <fieldset>
