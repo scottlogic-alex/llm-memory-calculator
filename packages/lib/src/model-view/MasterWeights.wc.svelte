d162aa3a76cc1836fb99980131647f12026870f2
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-09-25 16:46:11 +0100

    layer precision checkboxes working with store

--- packages/lib/src/model-view/MasterWeights.wc.svelte
+++ packages/lib/src/model-view/MasterWeights.wc.svelte
@@ -1,41 +1,53 @@
 <svelte:options tag="master-weights"/>
 
 <script lang="ts">
-    import { MasterWeightPrecision } from '../master-weights-model/master-weights';
-    import { masterWeightPrecision } from '../master-weights-store/master-weights';
-    import { LayerWeightPrecision, PrecisionHungryLayer } from '../master-weights-model/layer-weights';
-    import { layerWeights, setLayerWeightPrecision } from '../master-weights-store/layer-weights';
-    /*
-    fp32 or half-precision.
-    if half-precision, then provide fp32 options for LN, embedding, unembedding
-    */
+  import type { ChangeEventHandler } from 'svelte/elements';
+  import { MasterWeightPrecision } from '../master-weights-model/master-weights';
+  import { masterWeightPrecision } from '../master-weights-store/master-weights';
+  import { LayerWeightPrecision, type PrecisionHungryLayer } from '../master-weights-model/layer-weights';
+  import { layerWeights, setLayerWeightPrecision, type SetLayerWeightPrecision } from '../master-weights-store/layer-weights';
+  /*
+  fp32 or half-precision.
+  if half-precision, then provide fp32 options for LN, embedding, unembedding
+  */
+
+  const onChangeLayerPrecision: ChangeEventHandler<HTMLInputElement> = ({ currentTarget }) => {
+    const { value, checked } = currentTarget;
+    const layer: PrecisionHungryLayer = value as PrecisionHungryLayer;
+    const precision: LayerWeightPrecision = checked ? LayerWeightPrecision.float : LayerWeightPrecision.half;
+    const action: SetLayerWeightPrecision = {
+      layer,
+      precision,
+    }
+    setLayerWeightPrecision(action);
+  };
 </script>
 
 <label class="row">
-    Precision
-    <select
-      bind:value={$masterWeightPrecision}
-    >
-        {#each Object.values(MasterWeightPrecision) as precision}
-            <option value={precision}>{precision}</option>
-        {/each}
-    </select>
+  Precision
+  <select
+    bind:value={$masterWeightPrecision}
+  >
+    {#each Object.values(MasterWeightPrecision) as precision}
+      <option value={precision}>{precision}</option>
+    {/each}
+  </select>
 </label>
 {#if $masterWeightPrecision === MasterWeightPrecision.half}
   <div class="row">
     <!-- provide fp32 options for LN, embedding, unembedding -->
-    {#each Object.values(PrecisionHungryLayer) as layer}
-        <label class="full-width">
-          <input type="checkbox" checked={$layerWeights[layer] == LayerWeightPrecision.float}>
-          {layer}
-        </label>
+    {#each Object.keys($layerWeights) as layer}
+      <label class="form-control">
+        <input type="checkbox" checked={$layerWeights[layer] === LayerWeightPrecision.float} on:change|preventDefault={onChangeLayerPrecision} value={layer}>
+        {layer}: {$layerWeights[layer]}
+      </label>
     {/each}
   </div>
 {/if}
 
 <!-- <dl>
-    <dt>Hi</dt>
-    <dd>There</dd>
+  <dt>Hi</dt>
+  <dd>There</dd>
 </dl> -->
 
 <style>
@@ -54,7 +66,8 @@
     flex-wrap: wrap;
     gap: 1em;
   }
-  .full-width {
+  .form-control {
+    display: block;
     width: 100%;
   }
 </style>
