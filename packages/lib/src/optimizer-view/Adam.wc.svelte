766d51f6ec94d0f041ceabb892864fa13e7acd99
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-09-26 17:49:31 +0100

    be consistent with explicitly saying what layout flow we want

--- packages/lib/src/optimizer-view/Adam.wc.svelte
+++ packages/lib/src/optimizer-view/Adam.wc.svelte
@@ -16,23 +16,27 @@
 
 <div>
   <!-- Hi, I'm Adam -->
-  <div>
-    <h4 class="tight-below tight-above">Datatype</h4>
-    <div>
-      <label><input type="radio" bind:group={$adamDtype} value={AdamDataType.float32}/>32-bit</label>
+  <div class="row">
+    <div class="row">
+      <h4 class="tight-below tight-above">Datatype</h4>
     </div>
-    <div>
-      <label><input type="radio" bind:group={$adamDtype} value={AdamDataType.int8}/>8-bit</label>
+    <div class="row">
+      <label class="col-12"><input type="radio" bind:group={$adamDtype} value={AdamDataType.float32}/>32-bit</label>
+      <label class="col-12"><input type="radio" bind:group={$adamDtype} value={AdamDataType.int8}/>8-bit</label>
     </div>
   </div>
-  <div>
-    <h4 class="tight-below">Buffers</h4>
-    <dl>
-      <dt>Estimate of gradient mean</dt>
-      <dd>{grad_mean_estimate} bytes/param</dd>
-      <dt>Estimate of gradient variance</dt>
-      <dd>{grad_var_estimate} bytes/param</dd>
-    </dl>
+  <div class="row">
+    <div class="row">
+      <h4 class="tight-below">Buffers</h4>
+    </div>
+    <div class="row">
+      <dl class="col-12">
+        <dt>Estimate of gradient mean</dt>
+        <dd>{grad_mean_estimate} bytes/param</dd>
+        <dt>Estimate of gradient variance</dt>
+        <dd>{grad_var_estimate} bytes/param</dd>
+      </dl>
+    </div>
   </div>
 </div>
 
@@ -40,6 +44,20 @@
   dl {
     margin-top: 0;
     margin-bottom: 0.5em;
+  }
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    /* gap: 1em; */
+  }
+  .row > * {
+    flex-shrink: 0;
+    width: 100%;
+    max-width: 100%;
+  }
+  .col-12 {
+    flex: 0 0 auto;
+    width: 100%;
   }
   /* .form-group {
     margin-bottom: 0em;
