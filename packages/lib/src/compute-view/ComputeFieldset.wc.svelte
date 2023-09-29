41fb31c255d936f943450594dce05d13ad35667c
Alex Birch <scottlogic-alex@users.noreply.github.com>
2023-09-29 19:05:27 +0100

    more thoughts on how to compute grads

--- packages/lib/src/compute-view/ComputeFieldset.wc.svelte
+++ packages/lib/src/compute-view/ComputeFieldset.wc.svelte
@@ -40,6 +40,50 @@
   I have heard anecdotes that it's important for its weights to be 32-bit too.
   Check what Ramesh et al 2021 recommends.
   -->
+  <!--
+  Okay so basically
+  Master Weights 32-bit
+    uniform-precision:
+    - 16-bit compute copy for most layers
+    mixed-precision:
+    -
+  Master Weights mostly 16-bit, some 32-bit.
+    mixed-precision:
+    - mixed-precision applies to the 32-bit weights only
+ 
+  16-bit Master Weights & mixed-precision [arguably weird]:
+  ====
+  your "weight grad" is the compute copy's weight grad.
+  for the layernorm layer, if we choose to utilize its float32 weights, then that layer will output a float32 grad.
+  so,
+  mixed-precision..
+  master weights:
+  - 4 bytes/param
+  compute copy:
+  - 2 bytes/param
+  grads:
+  - 2 bytes/param
+
+  mixed-precision but float32 LayerNorm..
+  master weights:
+  - 4 bytes/param
+  compute copy:
+  - 2 bytes/param - 2 bytes per LayerNorm param
+  grads:
+  - 2 bytes/param + 2 bytes per LayerNorm param
+  or maybe I'm overthinking this. there's no API for you to say "keep my LayerNorm weights in float32"; **every** master weight
+  says it's in float32 initially.
+  so actually I think no intervention is required by the user here: this is probably exactly what AMP is doing already
+
+  uniform-precision, master mostly bfloat16, float32 LayerNorm..
+  master weights:
+  - 2 bytes/param + 2 bytes per LayerNorm param
+  grads:
+  - 2 bytes/param + 2 bytes per LayerNorm param
+  
+  not sure we should allow "mixed-precision, master mostly bfloat16, some layers float32". at best it would only treat embedding/unembedding as a special case,
+  and I don't have any anecdote of anybody doing that combination.
+  -->
   <div class="row">
     <dl class="col-12">
       <dt>Gradients</dt>
